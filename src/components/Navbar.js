29970850ad9418c77e79edf93ba04bc6afc92627
charly3212 <charly.2701997@gmail.com>
2024-05-22 15:23:53 -0500

    boton navegacion

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,45 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-white py-3">
       <div className="container px-5">
-        <a className="navbar-brand" href="/">
+        <Link className="navbar-brand" to="/">
           <span className="fw-bolder text-primary">Carlos Andrade</span>
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
+          onClick={toggleNavbar}
           aria-controls="navbarSupportedContent"
-          aria-expanded="false"
+          aria-expanded={isOpen}
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div
+          className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`}
+          id="navbarSupportedContent"
+        >
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <Link className="nav-link" to="/">
                 Inicio
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/resume">
+              <Link className="nav-link" to="/resume">
                 CV
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/projects">
+              <Link className="nav-link" to="/projects">
                 Redes Sociales
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/contact">
+              <Link className="nav-link" to="/contact">
                 Contacto
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
